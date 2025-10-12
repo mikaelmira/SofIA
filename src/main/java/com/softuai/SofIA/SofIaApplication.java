626f59986a2dd51b357b3f83dedf08ed858b9303
Mikael Mira <mmira.software@gmail.com>
2025-10-12 17:14:16 -0300

    first commit

---
+++ src/main/java/com/softuai/SofIA/SofIaApplication.java
@@ -0,0 +1,13 @@
+package com.softuai.SofIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softuai/SofIA/SofIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuai.SofIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
